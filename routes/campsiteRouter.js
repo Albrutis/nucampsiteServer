0d0fda727c53c77773c25e2518c8c53ff2aad679
Charles Watkins <Charleswatkins380@gmail.com>
2020-07-18 12:06:02 -0700

    Updated Week 3 workshop

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -239,20 +239,35 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
-  .delete(authenticate.verifyUser, (req, res, next) => {
-    Campsite.findById(req.params.campsiteId)
-      .then((campsite) => {
-        if (req.body.author.equals(req.user._id)) {
+  .delete(
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Campsite.findById(req.params.campsiteId)
+        .then((campsite) => {
           if (campsite && campsite.comments.id(req.params.commentId)) {
-            campsite.comments.id(req.params.commentId).remove();
-            campsite
-              .save()
-              .then((campsite) => {
-                res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
-                res.json(campsite);
-              })
-              .catch((err) => next(err));
+            //checking if ID is the same as the owner of the comment//
+            if (
+              req.user._id.equals(
+                campsite.comments.id(req.params.commentId).author._id
+              )
+            ) {
+              campsite.comments.id(req.params.commentId).remove();
+              campsite
+                .save()
+                .then((campsite) => {
+                  res.statusCode = 200;
+                  res.setHeader("Content-Type", "application/json");
+                  res.json(campsite);
+                })
+                .catch((err) => next(err));
+            } else {
+              err = new Error(
+                `${req.user_.id} is not the author of this comment!`
+              );
+              err.status = 403;
+              return next(err);
+            }
           } else if (!campsite) {
             err = new Error(`Campsite ${req.params.campsiteId} not found`);
             err.status = 404;
@@ -262,13 +277,9 @@ campsiteRouter
             err.status = 404;
             return next(err);
           }
-        } else {
-          err = new Error(`${req.user_.id} is not the author of this comment!`);
-          err.status = 403;
-          return next(err);
-        }
-      })
-      .catch((err) => next(err));
-  });
+        })
+        .catch((err) => next(err));
+    }
+  );
 
 module.exports = campsiteRouter;
